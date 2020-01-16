b70916563061ca7698dd94901d5093300b1a5c4c
Andreas Longva <longva@cs.rwth-aachen.de>
2020-01-16 08:43:41 +0100

    Allow intersections to work with different objects
    Only the envelopes are used for determining intersection candidates,
    so it suffices that the envelopes of the two objects are of
    the same type.

--- rstar/src/algorithm/intersection_iterator.rs
+++ rstar/src/algorithm/intersection_iterator.rs
@@ -4,18 +4,20 @@ use crate::RTreeNode;
 use crate::RTreeNode::*;
 use crate::RTreeObject;
 
-pub struct IntersectionIterator<'a, T>
+pub struct IntersectionIterator<'a, T, U=T>
 where
     T: RTreeObject,
+    U: RTreeObject,
 {
-    todo_list: Vec<(&'a RTreeNode<T>, &'a RTreeNode<T>)>,
+    todo_list: Vec<(&'a RTreeNode<T>, &'a RTreeNode<U>)>,
 }
 
-impl<'a, T> IntersectionIterator<'a, T>
+impl<'a, T, U> IntersectionIterator<'a, T, U>
 where
     T: RTreeObject,
+    U: RTreeObject<Envelope=T::Envelope>,
 {
-    pub(crate) fn new(root1: &'a ParentNode<T>, root2: &'a ParentNode<T>) -> Self {
+    pub(crate) fn new(root1: &'a ParentNode<T>, root2: &'a ParentNode<U>) -> Self {
         let mut intersections = IntersectionIterator {
             todo_list: Vec::new(),
         };
@@ -23,7 +25,7 @@ where
         intersections
     }
 
-    fn push_if_intersecting(&mut self, node1: &'a RTreeNode<T>, node2: &'a RTreeNode<T>) {
+    fn push_if_intersecting(&mut self, node1: &'a RTreeNode<T>, node2: &'a RTreeNode<U>) {
         if node1.envelope().intersects(&node2.envelope()) {
             self.todo_list.push((node1, node2));
         }
@@ -32,7 +34,7 @@ where
     fn add_intersecting_children(
         &mut self,
         parent1: &'a ParentNode<T>,
-        parent2: &'a ParentNode<T>,
+        parent2: &'a ParentNode<U>,
     ) {
         if !parent1.envelope().intersects(&parent2.envelope()) {
             return;
@@ -55,17 +57,23 @@ where
     }
 }
 
-impl<'a, T> Iterator for IntersectionIterator<'a, T>
+impl<'a, T, U> Iterator for IntersectionIterator<'a, T, U>
 where
     T: RTreeObject,
+    U: RTreeObject<Envelope=T::Envelope>
 {
-    type Item = (&'a T, &'a T);
+    type Item = (&'a T, &'a U);
 
     fn next(&mut self) -> Option<Self::Item> {
         while let Some(next) = self.todo_list.pop() {
             match next {
                 (Leaf(t1), Leaf(t2)) => return Some((&t1, &t2)),
-                (leaf @ Leaf(_), Parent(p)) | (Parent(p), leaf @ Leaf(_)) => {
+                (leaf @ Leaf(_), Parent(p)) => {
+                    p.children()
+                        .iter()
+                        .for_each(|c| self.push_if_intersecting(leaf, c));
+                },
+                (Parent(p), leaf @ Leaf(_)) => {
                     p.children()
                         .iter()
                         .for_each(|c| self.push_if_intersecting(c, leaf));
